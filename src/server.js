447752f425e8d1245f37c771c7799bff24fd58d6
JNickBaughan <JNickBaughan@gmail.com>
2020-07-05 16:17:24 -0400

    add graphQL init

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let bodyParser = require("body-parser")
+const { ApolloServer, gql} = require('apollo-server-express')
 let PORT = 3000;
 let server = express();
 const middlewares = [
@@ -9,6 +10,27 @@ const middlewares = [
 ];
 	
 server.use(...middlewares);
+
+const typeDefs = gql`
+  type Query {
+    testQuery(input: String): String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    testQuery: (_, args) => args.input
+  }
+}
+
+
+
+const graphQLServer = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+graphQLServer.applyMiddleware({ app: server });
 
 server.get("/", (req, res) => res.send(`<!DOCTYPE html>
 <html>
